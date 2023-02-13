3e979917b33a7e2f2712a203ad55bfffbecbaf4a
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-13 18:58:42 -0300

    POST route /products

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', productsController.listProducts);
 router.get('/:id', productsController.getProduct);
+router.post('/', productsController.createProduct);
 
 module.exports = router;
